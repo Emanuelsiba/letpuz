25e2fb38c700554044d669bd066aae872af37c7f
Emanuelsiba <emanuelsibanda49@gmail.com>
2025-03-07 09:38:23 +0200

    bean

--- main.cpp
+++ main.cpp
@@ -3,19 +3,28 @@
 #include <cstdlib>
 #include <termios.h>
 #include <unistd.h>
+#include <vector>
+#include <random>
+#include <algorithm>
+
+// char matrix[3][3] = {
+//     {'a', 'b', 'c'},
+//     {'d', 'e', 'f'},
+//     {'g', '_', 'h'}
+// };
 
 
-char matrix[3][3] = {
-    {'a', 'b', 'c'},
-    {'d', 'e', 'f'},
-    {'g', '_', 'h'}
-};
+char matrix[3][3];
 
 char matrix_orderned[3][3] = {
     {'a', 'b', 'c'},
     {'d', 'e', 'f'},
     {'g', 'h', '_'}
 };
+
+
+
+
 
 
 std::string get_key() {
@@ -75,12 +84,37 @@ void display_matrix(int moves) {
     
 }
 
+void shuffle_matrix() {
+    std::vector<char> elements = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    do {
+        std::shuffle(elements.begin(), elements.end(), gen);
+
+        int index = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 2 && j == 2) {
+                    matrix[i][j] = '_';
+                }
+                else {
+                    matrix[i][j] = elements[index++];
+                }
+                
+            }
+        }
+    } while (check_victory(matrix, matrix_orderned)); 
+}
+
+
 
 
 
 int main() {
 
     int moves = 0;
+    shuffle_matrix();
 
     while (true) {
         std::system("clear");
@@ -133,4 +167,3 @@ int main() {
 
 
 //end
-//
